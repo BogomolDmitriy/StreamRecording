0987814401969011c43f5301705aa50ad66c004b
Dmitriy Bohomol <bogomoldima1992@gmail.com>
2024-09-26 23:26:03 +0300

    start WPF

--- StreamRecording/MainWindow.xaml.cs
+++ StreamRecording/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,55 @@ namespace StreamRecording
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        string selectedFilePath;
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем диалоговое окно сохранения файла
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Настраиваем фильтр для типов файлов
+            saveFileDialog.Filter = "All files (*.*)|*.*";
+
+            // Показываем диалоговое окно и проверяем результат
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Если пользователь выбрал место и имя файла, выводим путь
+                selectedFilePath = saveFileDialog.FileName;
+                MessageBox.Show($"File will be saved at: {selectedFilePath}");
+            }
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Open clicked");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cut_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cut clicked");
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Copy clicked");
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Paste clicked");
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("About clicked");
         }
     }
 }
